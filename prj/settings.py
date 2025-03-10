538b538159b343302cb0c99f476093ee8ed2fd48
junior's signature <chibuzor.john.2018@gmail.com>
2025-03-10 00:51:09 +0000

    updated settings file

--- prj/settings.py
+++ prj/settings.py
@@ -118,13 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+STATIC_URL = "/staticfiles/"
 
-STATIC_ROOT = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
